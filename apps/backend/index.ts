c5a9f9be2b10495f146e78d6190c3167a34d88e3
ayush <ayushkumar2122.ak@gmail.com>
2026-06-10 21:28:47 +0530

    feat: postgres connection
    Signed-off-by: ayush <ayushkumar2122.ak@gmail.com>

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,16 +1,38 @@
 import express from 'express';
+import axios from "axios"
+import cors from 'cors';
 import { PreInterviewBody } from './types';
+import { prisma } from "./db"
+
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.post('/api/v1/pre-interview', (req, res) => {
+app.post('/api/v1/pre-interview',async (req, res) => {
   const {success, data} = PreInterviewBody.safeParse(req.body);
   if(!success) {
     return res.status(400).json({error: "Invalid data"});
   }
   const github = data.github.endsWith("/") ? data.github.slice(0,-1) : data.github;
-  const linkedIn = data.linkedIn.endsWith("/") ? data.linkedIn.slice(0,-1) : data.linkedIn;
+  const githubUserName = github.slice(19);
 
+  const userRepos = await axios.get(`https://api.github.com/users/${githubUserName}/repos`);
+  const scrap = userRepos.data.map((x:any) => ({
+    description : x.description,
+    name: x.name,
+    fullName: x.full_name,
+    starCount: x.stargazers_count
+  }))
+
+  const interviewId = await prisma.interview.create({
+    data:{
+      githubMetaData : JSON.stringify(scrap),
+      status: "Pre"
+    }
+  })
+  
+  res.status(200).json({ id: interviewId.id });
 });
 
 app.listen(3001, () => {
